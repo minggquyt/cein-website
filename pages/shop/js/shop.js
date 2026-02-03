f7bcb7412addaff0a69b6ead872960ce3577f339
Nguyễn Ngọc Hậu <nguyenngochau092@gmail.com>
2026-02-03 18:09:37 +0700

    update

--- pages/shop/js/shop.js
+++ pages/shop/js/shop.js
@@ -1,4 +1,4 @@
-fetch("../assets/data/products.json")
+fetch("../../assets/data/products.json")
     .then(response => response.json())
     .then(products => {
         const productList = document.getElementById('product-list');
@@ -6,8 +6,8 @@ fetch("../assets/data/products.json")
         products.forEach(product => {
             productItem += `
             <div class="product-item">
-                <a><img src="../assets/icon/Heart.png" alt="Like" class="heart-icon" style="width: 36px; height: 36px;"></a>
-                <a><img src="../assets/icon/Plus.png" alt="Cart" class="plus-icon" style="width: 36px; height: 36px;"></a>
+                <a><img src="../../assets/icon/Heart.png" alt="Like" class="heart-icon" style="width: 36px; height: 36px;"></a>
+                <a><img src="../../assets/icon/Plus.png" alt="Cart" class="plus-icon" style="width: 36px; height: 36px;"></a>
                 <img src="${product.image}" alt="${product.product_name}">
                 <h3>${product.product_name}</h3>
                 <p>$${product.price}</p>
@@ -15,3 +15,4 @@ fetch("../assets/data/products.json")
         })
         productList.innerHTML = productItem;
     })
+
