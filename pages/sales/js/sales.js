085d0fe5d199800b24d083cc44f1c3b5dd982511
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2026-03-01 15:26:26 +0700

    add cart page

---
+++ pages/sales/js/sales.js
@@ -0,0 +1,18 @@
+fetch("../../assets/data/new-arrivals.json")
+    .then(response => response.json())
+    .then(products => {
+        const productList = document.getElementById('product-list');
+        var productItem = '';
+        products.forEach(product => {
+            productItem += `
+            <a href="../product-detail/product-detail.html" class="product-item">
+                <img src="../../assets/icon/Heart.png" alt="Like" class="heart-icon" style="width: 36px; height: 36px;">
+                <img src="../../assets/icon/Plus.png" alt="Cart" class="plus-icon" style="width: 36px; height: 36px;">
+                <img src="${product.image}" alt="${product.product_name}">
+                <h3>${product.product_name}</h3>
+                <p>$${product.price}</p>
+            </a>`;
+        })
+        productList.innerHTML = productItem;
+    })
+
